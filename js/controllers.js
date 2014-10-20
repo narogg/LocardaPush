51641c8374a89b76813e26c1092ca309ac3bc0d7
Narog <gmandini@net.hr>
2014-10-20 14:14:33 +0200

    initial commit, api comunication established, get&post working

---
+++ js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('EnemiesCtrl', function($scope, Enemies) {
+  $scope.enemies = Enemies.all();
+  $scope.checkIn = function() {
+   alert('Call to Rails Locarda API');
+   /*TODO: checkIn user to clicked place*/
+   Enemies.postIt();
+   console.log('Enemies.postIt() called');
+   
+   
+   
+   
+   
+   }
+})
+
+.controller('EnemiesDetailCtrl', function($scope, $stateParams, Enemies) {
+  $scope.enemy = Enemies.get($stateParams.enemyId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
---
+++ js/services.js
@@ -0,0 +1,82 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Scruff McGruff' },
+    { id: 1, name: 'G.I. Joe' },
+    { id: 2, name: 'Miss Frizzle' },
+    { id: 3, name: 'Ash Ketchum' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+})
+
+.factory('Enemies', ['$http', function($http) {
+  // Might use a resource here that returns a JSON array
+	var enemies = this;
+	enemies = []; //init empty array
+	
+	$http.get('http://locarda.herokuapp.com/home/api').success(function(data){
+	//$http.get('http://localhost:3000/home/api').success(function(data){
+	enemies =  data;
+	});
+
+	return {
+		all: function() {
+	    return enemies;
+		},
+		// Simple index lookup  
+		get: function(enemyId){
+		return enemies[enemyId- 1]; //-1 needed for id lookup
+		},
+		
+	
+	/*Trying to post here*/
+	postIt : function(){
+	$http.post('http://localhost:3000/carts/checkin').success(function(data){
+	//$http.get('http://localhost:3000/home/api').success(function(data){
+	alert('Uspjesna prijava!');
+	}).error(function(data){alert ('Neuspjesna prijava!\nNapomenite osoblju.') })
+	}
+	/**/
+	
+	}
+	
+	}]
+	
+	);
+	//})();
+	
+	
+  // Some fake testing data
+  //var enemies = [
+  //  { id: 0, name: 'George Bush' },
+  //  { id: 1, name: 'PFITZER' },
+  //  { id: 2, name: 'Elly Lilly' },
+  //  { id: 3, name: 'Nestle' }
+  //];
+
+  //return {
+  //  all: function() {
+  //    return enemies;
+  //  },
+  //  get: function(enemyId) {
+  //    // Simple index lookup
+  //    return enemies[enemyId];
+  //  }
+  //}
+//});
