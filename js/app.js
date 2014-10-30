c9a81c7ebcde908b5b9b2fa6fd7e85dfa912977c
Narog <gmandini@net.hr>
2014-10-30 15:54:41 +0100

    delete authentication.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.authentication'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform, $ionicPopup) {
   $ionicPlatform.ready(function() {
